58e44ff2aa374fe49d5a595f8e4ad50335b0f62d
Rupanshi Chawda <rc80614@gmail.com>
2021-02-24 22:53:29 +0530

    footer added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,14 +6,18 @@ import '../App.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-          <video src="/videos/video-2.mp4" autoPlay loop muted />
+          <video src="/videos/video-1.mp4" autoPlay loop muted />
           <h1> ADVENTURE AWAITS </h1>
           <p> What are you waiting for? </p>
-          <div className="heo-btns">
-           <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' >
+          <div className="hero-btns">
+           <Button className='btns' 
+           buttonStyle='btn--outline' 
+           buttonSize='btn--large' >
              GET STARTED  
             </Button>   
-            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' >
+            <Button className='btns' 
+            buttonStyle='btn--primary' 
+            buttonSize='btn--large' >
              WATCH TRAILER <i className='far fa-play-circle' />
             </Button>   
           </div>            
